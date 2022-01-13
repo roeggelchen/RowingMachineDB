a7f0e3e4a128fbacc2736b4b66cce25ed3f8efe3
Simon Roggendorf <simon.roggendorf@gmail.com>
2022-01-13 11:53:23 +0100

    First running Version

--- RowingMachineDB/settings.py
+++ RowingMachineDB/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'RowingMachineDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rowingmachine',
+        'USER': 'roeggelchen',
+        'PASSWORD': 'bluskdjfob',
+        'HOST': '192.168.178.100',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
